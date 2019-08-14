eba76547a387917137e31009ba93c5d7fa2e1fbc
jmedveckyh <5638498+jmedveckyh@users.noreply.github.com>
2019-08-14 14:22:08 +0200

    host backend in php built-in dev server

---
+++ backend.js
@@ -0,0 +1,18 @@
+const php = require('node-php-server')
+const host = '127.0.0.1'
+const port = 80
+
+module.exports = {
+  hostname: host,
+  port: port,
+  serve: () => {
+    php.createServer({
+      hostname: host,
+      port: port,
+      base: './backend',
+      router: './kirby/router.php'
+    })
+
+    console.log(`Backend running at: http://${host}:${port}`)
+  }
+}
