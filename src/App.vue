59fd943e25bd194fff96859efda27ad3bc112fa0
jmedveckyh <5638498+jmedveckyh@users.noreply.github.com>
2019-08-16 14:13:48 +0200

    header ported to vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,164 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Note v-for="note in notes" :key="note.id" :note="note" />
+  <div id="app" class="page">
+    <Header :site="site" />
   </div>
 </template>
 
 <script>
-import Note from './components/Note.vue'
-
-const env = variable => process.env['VUE_APP_' + variable] || ''
+import Api from './api'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    Note
+    Header
   },
   data() {
     return {
-      notes: []
+      site: {}
     }
   },
-  created() {
-    const api = window.location.origin + env('BACKEND_PUBLIC_PATH') + '/api'
-    const auth = btoa(env('API_EMAIL') + ':' + env('API_PASSWORD'))
-    const headers = { Authorization: 'Basic ' + auth }
+  async created() {
+    const api = new Api()
 
-    let self = this
-
-    fetch(`${api}/pages/notes/children`, { headers: headers })
-      .then(response => response.json())
-      .then(result => {
-        self.notes = result.data
-      })
+    const site = await api.get('site?select=title,url,children')
+    document.title = site.title
+    site.children = site.children.filter(child => child.num)
+    this.site = site
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style>
+:root {
+  --content-width: 65rem;
+}
+
+*,
+*:after,
+*:before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  color: currentColor;
+  text-decoration: none;
+}
+
+strong,
+b {
+  font-weight: 500;
+}
+
+img {
+  width: 100%;
+}
+
+.page {
+  padding: 5vh 5vw 10vh;
+}
+.page > * {
+  max-width: var(--content-width);
+  margin: 0 auto;
+}
+
+main {
+  min-height: calc(100vh - 10rem);
+}
+
+.intro {
+  padding: 10vh 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.intro h1 {
+  position: relative;
+  margin-bottom: 1rem;
+  font-weight: 900;
+  font-size: calc(1vw + 2rem);
+  z-index: 1;
+}
+
+.tags {
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 0.075em;
+  font-size: 0.75rem;
+  font-weight: 600;
+}
+
+.text {
+  line-height: 1.5em;
+}
+.text p,
+.text figure,
+.text ul,
+.text ol {
+  margin-bottom: 1.5em;
+}
+.text h2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
+  text-align: center;
+}
+.text > *:first-child {
+  margin-top: 0;
+}
+.text a {
+  position: relative;
+  white-space: nowrap;
+  font-weight: 500;
+  z-index: 1;
+  display: inline-block;
+  border-bottom: 2px solid #000;
+}
+.text figure {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+}
+.text img {
+  width: 100%;
+}
+
+.footer {
+  padding: 1.5rem 5vw 10vh;
+  text-align: center;
+  max-width: var(--content-width);
+  margin: 0 auto;
+  line-height: 1.5em;
+}
+.footer a {
+  display: inline-block;
+  font-size: 0.875rem;
+}
+.footer > a {
+  margin-bottom: 1.5rem;
+  border-top: 2px solid #000;
+  width: 16.5rem;
+  padding-top: 0.5rem;
+}
+
+.social a {
+  margin: 0 0.75rem;
+  padding: 0.5rem 1rem;
+  border: 2px solid #000;
+  width: 7.5rem;
+}
+.social a:hover {
+  background: #000;
+  color: #fff;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="header">
+    <a class="logo" :href="site.url">{{ site.title }}</a>
+
+    <nav id="menu" class="menu">
+      <a v-for="page in site.children" :key="page.id" :href="page.url">
+        {{ page.title }}
+      </a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['site']
+}
+</script>
+
+<style>
+.header {
+  margin-bottom: 1.5rem;
+}
+
+.header a {
+  position: relative;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  letter-spacing: 0.05em;
+  padding: 0.5rem 0;
+  font-weight: 700;
+}
+
+.header .logo {
+  display: block;
+  margin-bottom: 1.5rem;
+  padding: 0.5rem 0;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.menu a {
+  margin: 0 0.75rem;
+}
+
+.menu a[aria-current] {
+  border-bottom: 2px solid #000;
+}
+
+@media screen and (min-width: 40rem) {
+  .header .logo {
+    margin-bottom: 0;
+  }
+  .header {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .menu {
+    margin-right: -0.75rem;
+  }
+}
+</style>
