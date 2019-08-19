256445edc00d80c59d74da508b6c0d1439dd1ab0
jmedveckyh <5638498+jmedveckyh@users.noreply.github.com>
2019-08-19 14:07:25 +0200

    default public path bug fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const backend = require('./backend.js')
 
 module.exports = {
-  publicPath: process.env.VUE_APP_FRONTEND_PUBLIC_PATH || '',
+  publicPath: process.env.VUE_APP_FRONTEND_PUBLIC_PATH || '/',
   outputDir: './frontend',
   productionSourceMap: false,
   devServer: {
