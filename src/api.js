fba9b28d02656ba6d7c138e4ddcb1cddc4f89517
jmedveckyh <5638498+jmedveckyh@users.noreply.github.com>
2019-08-23 16:51:17 +0200

    added commonly used methods to api class

--- src/api.js
+++ src/api.js
@@ -12,4 +12,33 @@ export default class Api {
     const json = await resp.json()
     return json.data
   }
+
+  async getPage(pageId) {
+    const page = await this.get(`pages/${pageId}?select=content`)
+    return page.content
+  }
+
+  async getListedChildren(pageId) {
+    const children = await this.get(`pages/${pageId}/children?select=id,num,content`)
+    return children.filter(child => child.num)
+  }
+
+  async getFiles(pageId) {
+    return await this.get(`pages/${pageId}/files?select=url,type,link,content`)
+  }
+
+  async getFileContent(link) {
+    const file = await this.get(`${link.substr(1)}?select=content`)
+    return file.content
+  }
+
+  async getFileProcessed(link, method, w, h) {
+    const url = link.replace('pages/', '').replace('files/', '')
+    return await this.get(`process/${method}/${url.substr(1)}?w=${w}&h=${h}`)
+  }
+
+  async getKirbyText(pageId, ...fields) {
+    const select = fields.map(field => field).join(',')
+    return await this.get(`kt/${pageId}?select=${select}`)
+  }
 }
