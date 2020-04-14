40c4a6f52722214a35b98cc6f83c93e8a3dea964
jmheretik <5638498+jmheretik@users.noreply.github.com>
2020-04-14 21:38:18 +0200

    returning .json manually because / and non existing paths are buggy and dont return jsons

--- kirby/site/plugins/kirby-vue-starterkit/routes.php
+++ kirby/site/plugins/kirby-vue-starterkit/routes.php
@@ -1,13 +1,19 @@
 <?php
 
 /**
- * Redirect all html templates to vue index (https://router.vuejs.org/guide/essentials/history-mode.html).
+ * Redirect all non-json templates to vue index (https://router.vuejs.org/guide/essentials/history-mode.html).
  * Taken from https://getkirby.com/docs/guide/routing#defining-your-own-routes
  */
 return [
     [
-        // match everything that doesn't end with .json
-        'pattern' => ['', '(.*)(?<!\.(json|nuxt))'],
+        'pattern' => ['.json', '(:all).json'],
+        'action'  => function ($pageId = 'home') {
+            kirby()->response()->json();
+            return (page($pageId) ?? page('error'))->render([], 'json');
+        }
+    ],
+    [
+        'pattern' => ['(.*)'],
         'action'  => function ($pageId = 'home') {
             return tpl::load(kirby()->roots()->snippets() . DS . 'vue-index.php', ['site' => site(), 'page' => page($pageId) ?? page('error')], false);
         }
